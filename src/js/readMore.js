40a3904f5a5013fae7b614ef2def7c15ddd7a1e3
Illia Karpoff <hawaii2001@ukr.net>
2020-06-20 02:36:36 +0300

    footer_nachalo

--- src/js/readMore.js
+++ src/js/readMore.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   function moreLess(initiallyVisibleCharacters) {
     var visibleCharacters = initiallyVisibleCharacters;
     var paragraph = $(".text")
 
 
-    paragraph.each(function() {
+    paragraph.each(function () {
       var text = $(this).text();
       var wholeText = text.slice(0, visibleCharacters) + "<span class='ellipsis'>... </span><a href='#' class='more'>Читать дальше</a>" + "<span style='display:none'>" + text.slice(visibleCharacters, text.length) + "<a href='#' class='less'> Скрыть</span>"
 
@@ -15,25 +15,25 @@ $(document).ready(function() {
         $(this).html(wholeText)
       }
     });
-    $(".more").on('click', function(e) {
+    $(".more").on('click', function (e) {
       e.preventDefault();
       $(this).hide().prev().hide();
       $(this).next().show();
     });
-    $(".less").on('click', function(e) {
+    $(".less").on('click', function (e) {
       e.preventDefault();
       $(this).parent().hide().prev().show().prev().show();
     });
   };
 
 
-    if(window.screen.width>=100 && window.screen.width<=768){
-      moreLess(167);
-      console.log("width>=100");
-    }else if(window.screen.width>=768){
-      moreLess(310);
-      console.log("width>=768");
-    }
+  if (window.screen.width >= 100 && window.screen.width <= 768) {
+    moreLess(167);
+    console.log("width>=100");
+  } else if (window.screen.width >= 768) {
+    moreLess(310);
+    console.log("width>=768");
+  }
 
 
 
