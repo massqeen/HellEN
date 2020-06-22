f97bc32256d21eb11857c28ea092e29a2845f55e
massqeen <62159695+massqeen@users.noreply.github.com>
2020-06-22 21:11:47 +0300

    fixed bug with jumping elements on body lock, added blocking empty links to not jump to header

--- src/js/q-burger.js
+++ src/js/q-burger.js
@@ -1,17 +1,20 @@
+const notLinks = document.querySelectorAll('.link__not-active');
 function burgerMenu() {
   const burger = document.querySelector('.header__burger'),
     menu = document.querySelector('.header__menu'),
     logo = document.querySelector('.logo'),
     links = document.querySelectorAll('.header__link');
-  // notlinks = document.querySelectorAll('.link__not-active');
+
   function toggleMenu() {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
     logo.classList.toggle('active');
     if (menu.classList.contains('active')) {
-      document.body.style.overflow = 'hidden';
+      // document.body.style.overflow = 'hidden';
+      bodyLock();
     } else {
-      document.body.style.overflow = 'visible';
+      // document.body.style.overflow = 'visible';
+      bodyUnLock();
     }
   }
 
@@ -23,7 +26,7 @@ function burgerMenu() {
   });
 }
 
-// [].forEach.call(notLinks, function (el) {
-//   el.addEventListener('click', () => preventDefault());
-// });
+[].forEach.call(notLinks, function (el) {
+  el.addEventListener('click', e => e.preventDefault());
+});
 burgerMenu();
