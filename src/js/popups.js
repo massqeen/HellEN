1dacdd7e9346ed90df8674ac2714321fe7100474
massqeen <62159695+massqeen@users.noreply.github.com>
2020-06-22 12:00:48 +0300

    added all necessary files and code to make popups work

--- src/js/popups.js
+++ src/js/popups.js
@@ -97,3 +97,30 @@ document.addEventListener('keydown', function (e) {
     popupClose(popupActive);
   }
 });
+
+// полифилы на closest и matches
+(function () {
+  // проверяем поддержку
+  if (!Element.prototype.closest) {
+    // реализуем
+    Element.prototype.closest = function (css) {
+      var node = this;
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+})();
+(function () {
+  // проверяем поддержку
+  if (!Element.prototype.matches) {
+    // определяем свойство
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+  }
+})();
