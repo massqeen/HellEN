4cb42cdaf281f6c8b69b77470d1ddbe197145d23
massqeen <62159695+massqeen@users.noreply.github.com>
2020-06-23 15:31:35 +0300

    added animation on scrolling

---
+++ src/js/scrollReveal.js
@@ -0,0 +1,68 @@
+ScrollReveal({
+  origin: 'top',
+  duration: 1500,
+  scale: 0.2,
+  easing: 'ease-in-out',
+  viewFactor: 0.3,
+  reset: true,
+}).reveal('.link-action--animate');
+
+ScrollReveal({
+  origin: 'left',
+  distance: '30rem',
+  duration: 2000,
+  easing: 'ease-in-out',
+}).reveal('.title-section');
+
+ScrollReveal({
+  origin: 'right',
+  distance: '20rem',
+  duration: 1500,
+  interval: 300,
+  scale: 0.2,
+  easing: 'ease-in-out',
+  viewFactor: 0.3,
+}).reveal('.card-anim-right');
+
+ScrollReveal({
+  origin: 'left',
+  distance: '20rem',
+  duration: 1500,
+  interval: 300,
+  scale: 0.2,
+  easing: 'ease-in-out',
+  viewFactor: 0.3,
+}).reveal('.card-anim-left');
+
+ScrollReveal({
+  origin: 'right',
+  distance: '50rem',
+  duration: 2000,
+  easing: 'ease-in-out',
+}).reveal('.program-section__title');
+
+ScrollReveal({
+  origin: 'right',
+  distance: '50rem',
+  duration: 2000,
+  easing: 'ease-in-out',
+}).reveal('.guarantee__head');
+
+ScrollReveal({
+  origin: 'right',
+  distance: '50rem',
+  duration: 2000,
+  interval: 300,
+  delay: 0.1,
+  scale: 0.2,
+  rotate: { x: 90, y: 90, z: 90 },
+  easing: 'ease-in-out',
+  viewFactor: 0.3,
+}).reveal('.program-section__list-item-title');
+
+ScrollReveal({
+  origin: 'right',
+  distance: '50rem',
+  duration: 2000,
+  easing: 'ease-in-out',
+}).reveal('.teacher__head');
